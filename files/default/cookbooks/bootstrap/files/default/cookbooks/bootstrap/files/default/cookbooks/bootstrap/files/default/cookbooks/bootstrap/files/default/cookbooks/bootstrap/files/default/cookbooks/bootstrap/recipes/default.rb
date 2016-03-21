5a466792c89d186a0657971d95442dd25acf0887
Luis De Siqueira <louthebrew@gmail.com>
2016-03-21 12:18:00 -0700

    init

--- files/default/cookbooks/bootstrap/files/default/cookbooks/bootstrap/files/default/cookbooks/bootstrap/files/default/cookbooks/bootstrap/files/default/cookbooks/bootstrap/files/default/cookbooks/bootstrap/recipes/default.rb
+++ files/default/cookbooks/bootstrap/files/default/cookbooks/bootstrap/files/default/cookbooks/bootstrap/files/default/cookbooks/bootstrap/files/default/cookbooks/bootstrap/files/default/cookbooks/bootstrap/recipes/default.rb
@@ -1,26 +1,26 @@
 file '/tmp/happyhost' do
   content "gobblygook-#{node[:set_fqdn]}"
 end
-#include_recipe 'hostname'
-#bootstrap 'this_node' do
-#  knife_pem_file  node[:bootstrap][:knife][:key][:file]
-#  knife_config_file  node[:bootstrap][:knife][:config]
-#  knife_config_dir  node[:bootstrap][:knife][:dir]
-#  knife_user  node[:bootstrap][:knife][:user]
-#  client_config_file  node[:bootstrap][:client][:config][:file]
-#  client_pem_file  node[:bootstrap][:client][:pem][:file]
-#  client_pem_str  node[:bootstrap][:client][:pem][:str]
-#  client_log_file  node[:bootstrap][:client][:log][:file]
-#  first_run_file  node[:bootstrap][:json][:file]
-#  validator_pem_str  node[:bootstrap][:validator][:pem][:str]
-#  validator_name  node[:bootstrap][:validator][:name]
-#  validator_file  node[:bootstrap][:validator][:file]
-#  chef_conf_dir  node[:bootstrap][:dir][:conf]
-#  chef_log_dir  node[:bootstrap][:dir][:log]
-#  databag_key_str  node[:bootstrap][:databag][:key][:str]
-#  databag_file  node[:bootstrap][:databag][:key][:file]
-#  chef_server_url  node[:bootstrap][:server][:url]
-#  chef_environment  node[:bootstrap][:environment]
-#  run_list  node[:bootstrap][:run_list]
-#  fqdn "#{node[:bootstrap][:hostname]}#{node[:bootstrap][:domain]}"
-#end
+include_recipe 'hostname'
+bootstrap 'this_node' do
+  knife_pem_file  node[:bootstrap][:knife][:key][:file]
+  knife_config_file  node[:bootstrap][:knife][:config]
+  knife_config_dir  node[:bootstrap][:knife][:dir]
+  knife_user  node[:bootstrap][:knife][:user]
+  client_config_file  node[:bootstrap][:client][:config][:file]
+  client_pem_file  node[:bootstrap][:client][:pem][:file]
+  client_pem_str  node[:bootstrap][:client][:pem][:str]
+  client_log_file  node[:bootstrap][:client][:log][:file]
+  first_run_file  node[:bootstrap][:json][:file]
+  validator_pem_str  node[:bootstrap][:validator][:pem][:str]
+  validator_name  node[:bootstrap][:validator][:name]
+  validator_file  node[:bootstrap][:validator][:file]
+  chef_conf_dir  node[:bootstrap][:dir][:conf]
+  chef_log_dir  node[:bootstrap][:dir][:log]
+  databag_key_str  node[:bootstrap][:databag][:key][:str]
+  databag_file  node[:bootstrap][:databag][:key][:file]
+  chef_server_url  node[:bootstrap][:server][:url]
+  chef_environment  node[:bootstrap][:environment]
+  run_list  node[:bootstrap][:run_list]
+  fqdn "#{node[:bootstrap][:hostname]}#{node[:bootstrap][:domain]}"
+end
